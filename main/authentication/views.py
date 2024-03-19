025fb0603a31755a4688f516498c63a8ebae936c
anastassow <127148449+anastassow@users.noreply.github.com>
2024-03-19 23:16:29 +0200

    login and logout

--- main/authentication/views.py
+++ main/authentication/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators  import api_view
 from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 
 # Create your views here.
 @api_view(['POST'])
@@ -15,3 +17,19 @@ def register(request,):
     else:
         new_user = User.objects.create(username=username, password=make_password(password))
         return Response("You have created a new account!")
+    
+@api_view(['POST'])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(request=request, username = username, password = password)
+    if user is not None:
+        login(request, user)
+        return Response("Hello, {username} you have loged into your account!")
+    else:
+        return Response("There is not a user with this username and password!")
+    
+@api_view(['POST'])
+def logout_view(request):
+    logout(request)
+    return Response("You have been logged out.")
