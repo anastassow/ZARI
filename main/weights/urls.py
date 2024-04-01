86d4b8a6b43fbbef9509b0ace5a929e366f55398
anastassow <127148449+anastassow@users.noreply.github.com>
2024-04-01 23:58:41 +0300

    ddz

--- main/weights/urls.py
+++ main/weights/urls.py
@@ -4,5 +4,5 @@ from .views import create_exercise, add_weight_with_reps, get_user_exercise_data
 urlpatterns = [
     path('exercise/', create_exercise, name = 'exercise'),
     path('weight/', add_weight_with_reps, name = 'weight'),
-    path('get_user_exercise_data/', get_user_exercise_data, name='get_user_exercise_data'),
+    path('get_user_exercise_data/', get_user_exercise_data, name='get_user_exercise_data')
 ]
